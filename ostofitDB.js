40a6f3cb95917e7c215be4b2ad814973ac248fcc
Robert <robertostovic15@gmail.com>
2023-06-22 16:10:02 +0200

    update

--- ostofitDB.js
+++ ostofitDB.js
@@ -1,9 +1,20 @@
 const { Sequelize } = require("sequelize");
 
-const ostofitDB = new Sequelize("ostofit", "kek", "123456", {
-  host: "localhost",
-  port: "1433",
-  dialect: "mssql",
-});
+// const ostofitDB = new Sequelize("ostofit", "kek", "123456", {
+//   host: "localhost",
+//   port: "1433",
+//   dialect: "mssql",
+// });
+
+const ostofitDB = new Sequelize(
+  "ostofit",
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mssql",
+  }
+);
 
 module.exports = ostofitDB;
